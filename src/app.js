0689bfc3bd0d9f0e634628e88ece3beaf07cec26
Jason <jason@localhost.com>
2025-06-01 23:46:57 -0500

    modified:   app.js

--- src/app.js
+++ src/app.js
@@ -32,9 +32,12 @@ app.set("views", "./src/views");
 
 setupMiddleware(app);
 
-app.listen(3400, () => {
+port = process.env.PORT || 3400
+
+app.listen(port, () => {
   console.log("Server listening on http://localhost:3400");
 });
+
 // Global error handlers for uncaught exceptions and rejections
 process.on("uncaughtException", (err) => {
   console.error("Uncaught Exception:", err);
