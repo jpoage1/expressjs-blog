09a719e9815e7e3471dc0e271b05c49d462634ba
Jason <jason@pi.jasonpoage.com>
2025-06-02 01:21:03 -0500

    new file:   public/css/base.css 	new file:   public/css/footer.css 	new file:   public/css/header.css 	new file:   public/css/layout.css 	new file:   public/css/main.css 	new file:   public/css/menu.css 	new file:   public/css/nav.css 	new file:   public/css/reset.css 	new file:   public/css/responsiveness.css 	new file:   public/css/sidebar.css 	modified:   public/css/styles.css 	modified:   src/middleware/formatHtml.js 	modified:   src/views/partials/postsMenu.handlebars

--- src/middleware/formatHtml.js
+++ src/middleware/formatHtml.js
@@ -4,19 +4,25 @@ const beautify = require("js-beautify").html;
 module.exports = function (req, res, next) {
   const originalSend = res.send;
 
-     res.send = function (body) {
-       if (
-         typeof body === "string" &&
-         res.get("Content-Type")?.includes("text/html")
-       ) {
-         body = beautify(body, {
-           indent_size: 2,
-           wrap_line_length: 80,
-           end_with_newline: true,
-         });
-       }
-       return originalSend.call(this, body);
-     };
+  res.send = function (body) {
+    const contentType = res.get("Content-Type") || "";
+    const isHTML = contentType.includes("text/html") || typeof body === "string" && body.trim().startsWith("<");
+
+    if (isHTML) {
+      try {
+        body = beautify(body, {
+          indent_size: 2,
+          wrap_line_length: 80,
+          end_with_newline: true,
+        });
+      } catch (e) {
+        console.error("Beautify error:", e);
+      }
+    }
+
+    return originalSend.call(this, body);
+  };
 
   next();
 };
+
