ec572ae527669d2e8d1f63707b8d21c682111b14
Jason <jason@localhost.com>
2025-06-03 11:29:57 -0500

    modified:   ecosystem.config.js 	modified:   package.json 	modified:   public/css/nav.css 	new file:   public/css/post.css 	modified:   src/views/layouts/main.handlebars 	modified:   src/views/pages/post.handlebars

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,8 @@ module.exports = {
       NODE_ENV: "development"
     },
     env_production: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      PORT: 3000
     },
     log_date_format: "YYYY-MM-DD HH:mm Z",
     error_file: "./logs/err.log",
