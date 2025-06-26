e0de6c896f9992cb171bb178407b07f2ba24a199
Jason <jason@localhost.com>
2025-06-26 15:58:46 -0500

    modified:   content 	modified:   public/css/styles.css 	modified:   src/css/submenu.css 	modified:   src/routes/about.js 	modified:   src/utils/baseContext.js 	deleted:    src/utils/constructionPage.js 	deleted:    src/utils/markdownPage.js 	modified:   src/views/partials/headers.handlebars

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,9 +1,5 @@
 // src/routes/about.js
 const express = require("express");
 const router = express.Router();
-const markdownPage = require("../utils/markdownPage");
-const constructionPage = require("../utils/constructionPage");
 
-markdownPage("/about/me", "about-me");
-constructionPage("/about/blog", "About this blog");
 module.exports = router;
