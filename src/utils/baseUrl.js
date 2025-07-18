ca9cb25d374592454234da6457150d92a5013347
Jason <jason@localhost.com>
2025-07-18 03:28:27 -0500

    modified:   content 	modified:   src/routes/tags.js 	modified:   src/services/sitemapService.js 	modified:   src/services/tagsService.js 	new file:   src/utils/hash.js 	modified:   src/utils/postFileUtils.js 	modified:   src/views/pages/sitemap.handlebars 	modified:   src/views/pages/tag-posts.handlebars 	modified:   src/views/pages/tags.handlebars 	modified:   .githooks/pre-push 	modified:   src/services/sitemapService.js 	modified:   src/utils/qualifyLinks.js 	modified:   test/routes.test.js 	modified:   src/utils/baseUrl.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,14 +1,16 @@
 // src/utils/baseUrl.js
-function getBaseUrl({ schema = null, host = null } = {}) {
+function getBaseUrl({ schema = null, host = null, port = null } = {}) {
   const envSchema = process.env.TEST_SCHEMA || process.env.SERVER_SCHEMA;
   const envDomain = process.env.TEST_DOMAIN || process.env.SERVER_DOMAIN;
+  const envPort = process.env.TEST_PORT || process.env.SERVER_PORT;
 
+  const finalPort = envPort || port || 3000;
   const finalProtocol = envSchema || schema || "https";
   const finalDomain = (envDomain || host || "localhost")
     .replace(/^https?:\/\//, "")
     .replace(/\/$/, "");
 
-  return `${finalProtocol}://${finalDomain}`;
+  return `${finalProtocol}://${finalDomain}${finalPort != 80 ? `:${finalPort}` : ""}`;
 }
 const baseUrl = getBaseUrl();
 
