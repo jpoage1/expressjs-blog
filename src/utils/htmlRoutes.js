49c7bb3d725f080aa7522194fa41b0f46aae810e
Jason Poage <jason@jasonpoage.com>
2026-03-21 21:53:13 -0500

    modified:   content 	modified:   package.json 	modified:   src/utils/htmlRoutes.js 	modified:   yarn.lock

--- src/utils/htmlRoutes.js
+++ src/utils/htmlRoutes.js
@@ -9,6 +9,8 @@ const { baseUrl } = require("./baseUrl.js");
 class HtmlRoutes extends BaseRoute {
   constructor() {
     super();
+    this.shouldCache = process.env.NODE_ENV === "production";
+    this.cache = new Map();
   }
 
   /**
@@ -23,56 +25,60 @@ class HtmlRoutes extends BaseRoute {
       `../../content/html/${contentFolder}`,
     );
     const configPath = path.join(folderPath, `config.yaml`);
+    // The router's endpoint
+    const assetsRouterPath = path.join(routePath, "assets");
+    this.router.use(assetsRouterPath, express.static(folderPath));
 
     const configRaw = await fs.readFile(configPath, "utf8");
     const pageConfig = yaml.load(configRaw);
 
-    // Fetch the actual HTML file name from the YAML config
-    const filePath = path.join(folderPath, pageConfig.file);
-    const htmlContent = await fs.readFile(filePath, "utf8");
-
-    const assetsDir = path.join(
-      __dirname,
-      `../../content/html/${pageConfig.slug}`,
-    );
-
-    // The router's endpoint
-    const assetsRouterPath = path.join(routePath, "assets");
-
-    // -- The uri
-    const assetsUri = baseUrl + assetsRouterPath;
-
-    const extraStyles = pageConfig.styles.map(
-      (stylePath) => assetsUri + "/" + stylePath,
-    );
-    const extraScripts = pageConfig.scripts.map(
-      (scriptsPath) => assetsUri + "/" + scriptsPath,
-    );
-
-    const context = {
-      title: pageConfig.title || "Default Title",
-      slug: routePath,
-      content: htmlContent,
-      extraStyles: extraStyles || [],
-      extraScripts: extraScripts || [],
-      meta: pageConfig.meta || {},
-    };
-
-    /*
-    console.log("Assets Router Path", assetsRouterPath);
-    console.log("Assets URI", assetsUri);
-    console.log("Assets Dir", assetsDir);
-    console.log("extraScripts", extraScripts);
-    console.log("extraStyles", extraStyles);
-    */
-
-    this.router.use(assetsRouterPath, express.static(assetsDir));
     this.router.get(routePath, async (req, res, next) => {
       // Renders the web page on each request
       // Moving the logic outside of the function would read contents ahead of time
       // Keeping the logic intact would allow editing files without reloading the server
       try {
-        res.renderWithBaseContext(`pages/${pageConfig.page}`, context);
+        // Return cached version if enabled and exists
+        if (this.shouldCache && this.cache.has(routePath)) {
+          return res.renderWithBaseContext(
+            this.cache.get(routePath).template,
+            this.cache.get(routePath).context,
+          );
+        }
+        // Fetch the actual HTML file name from the YAML config
+        const filePath = path.join(folderPath, pageConfig.file);
+        const htmlContent = await fs.readFile(filePath, "utf8");
+
+        // -- The uri
+        const assetsUri = baseUrl + assetsRouterPath;
+
+        const extraStyles = pageConfig.styles.map(
+          (stylePath) => assetsUri + "/" + stylePath,
+        );
+        const extraScripts = pageConfig.scripts.map(
+          (scriptsPath) => assetsUri + "/" + scriptsPath,
+        );
+
+        const context = {
+          title: pageConfig.title || "Default Title",
+          slug: routePath,
+          content: htmlContent,
+          extraStyles: extraStyles || [],
+          extraScripts: extraScripts || [],
+          meta: pageConfig.meta || {},
+        };
+        /*
+          console.log("Assets Router Path", assetsRouterPath);
+          console.log("Assets URI", assetsUri);
+          console.log("extraScripts", extraScripts);
+          console.log("extraStyles", extraStyles);
+          */
+        const template = `pages/${pageConfig.page}`;
+
+        // 4. Update Cache
+        if (this.shouldCache) {
+          this.cache.set(routePath, { template, context });
+        }
+        res.renderWithBaseContext(template, context);
       } catch (err) {
         err.statusCode = 500;
         next(err);
