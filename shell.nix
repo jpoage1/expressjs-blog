bc1217ddf7aa86cea83098b39f4ff9f040ec72c7
Jason Poage <jason@jasonpoage.com>
2026-04-01 17:58:37 -0500

    new file:   default.nix 	new file:   flake.nix 	modified:   shell.nix

--- shell.nix
+++ shell.nix
@@ -5,11 +5,15 @@ in
   pkgs.mkShell {
     inputsFrom = [deployment_pipeline];
     shellHook = ''
+      export PYTHONPATH="$PYTHONPATH:$(pwd)/src"
       export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
       export PUPPETEER_EXECUTABLE_PATH=${pkgs.chromium}/bin/chromium
+
       alias mkicon="node src/render-favicon.js"
       alias mkfavicons="node src/generate-favicon.js"
       alias mkfavicon="magick static/favicons/favicon-512.png -define icon:auto-resize=64,48,32,16 static/favicons/favicon.ico"
+
+      echo
       echo "Type 'mkicon' to render the favicon-512.png"
       echo "Type 'mkfavicon' to generate favicon.ico from favicon-512.png"
 
