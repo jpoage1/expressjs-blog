64132da5335a21dd21558bd57aea5623bb3b3dac
Jason Poage <jason@jasonpoage.com>
2026-06-01 17:11:59 -0500

    modified:   src/db/config.js

--- src/db/config.js
+++ src/db/config.js
@@ -1,11 +1,23 @@
 function buildConfig() {
   const config = require("#config");
   // config.dbUrl takes priority; falls back to individual connection fields.
-  return config.dbUrl
+
+  const { db } = config;
+  const max = db.max || 6;
+
+  const dbUrl = config.dbUrl || db.url;
+
+  return dbUrl
     ? {
-        connectionString: config.dbUrl,
-        max: config.db.max || 6,
+        connectionString: dbUrl,
       }
-    : config.db;
+    : {
+        host: db.host,
+        port: db.port,
+        user: db.user,
+        password: db.password,
+        database: db.database,
+        max,
+      };
 }
 module.exports = buildConfig();
